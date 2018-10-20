07cdab5546170e64cd4d070ee5d6a488dafd7c3a
z12603 <z12603@163.com>
2018-10-20 19:58:36 +0800

    Update wechat/tests.py

--- wechat/tests.py
+++ wechat/tests.py
@@ -1,3 +1,21 @@
 from django.test import TestCase
+<<<<<<< HEAD
 
 # Create your tests here.
+=======
+from django.test.client import Client
+from django.utils import timezone
+from django.contrib.auth.models import User
+from wechat.models import *
+# Create your tests here.
+
+# WeChat测试
+class WechatGetTest(TestCase):
+    def setUp(self):
+        User.objects.create_superuser(username='root', email='xz1526003@163.com', password='xqws2018')
+
+    # get请求
+    def test_login_get(self):
+        response = self.client.get('/api/a/login')
+        self.assertEqual(response.json()['code'], 0)
+>>>>>>> origin/hyou
